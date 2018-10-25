0ca65f8317af649a48e8bf2eb5d543553dcb3995
Varun Nivas <30322599+froztedfate@users.noreply.github.com>
2018-10-25 23:09:45 +0530

    New commit

--- Hard/Twitter/tweet.py
+++ Hard/Twitter/tweet.py
@@ -1,20 +1,14 @@
-
-# importing the module 
 import tweepy 
   
-# personal details 
 consumer_key ="2URvdjvosSqkVE99zou14s0j2"
 consumer_secret ="OiOAEhsknVsYtn7lEoxAF0zTQYPasJ2CMhjJndagZAn0xOBldE"
 access_token ="1423408446-r6nt1a8rmc3yKi7xUUjv521JC8IMJAwyWdpCOiW"
 access_token_secret ="KmFpOa7jkyxW3K8B4kWxD6Rj4tl3bEChQTGgI5bI9BO2E"
   
-# authentication of consumer key and secret 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
   
-# authentication of access token and secret 
 auth.set_access_token(access_token, access_token_secret) 
 api = tweepy.API(auth) 
   
-# update the status 
 api.update_status(status ="Hello Everyone !!") 
 

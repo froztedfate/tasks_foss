0042f15cd525b73789a7f50d0b9b6c551754b330
froztedfate <froztedfate@gmail.com>
2018-10-26 04:20:19 +0530

    New Update

---
+++ Hard/Twitter/tockens.py
@@ -0,0 +1,15 @@
+Consumer API keys
+
+2URvdjvosSqkVE99zou14s0j2 (API key)
+
+OiOAEhsknVsYtn7lEoxAF0zTQYPasJ2CMhjJndagZAn0xOBldE (API secret key)
+
+
+Access token & access toke
+n secret
+
+1423408446-r6nt1a8rmc3yKi7xUUjv521JC8IMJAwyWdpCOiW (Access token)
+
+KmFpOa7jkyxW3K8B4kWxD6Rj4tl3bEChQTGgI5bI9BO2E (Access token secret)
+
+Read and write (Access level
